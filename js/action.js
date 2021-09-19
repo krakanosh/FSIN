526f20a798b9f01ab41fa4f9357b59cbab803d06
Lisin Alexandr <berserker37rus@gmail.com>
2021-09-19 05:58:31 +0300

    v0.1.3.3

--- js/action.js
+++ js/action.js
@@ -1,3 +1,5 @@
-(function () {
-
-})();
+// Скрытие формы после отправки и вывод сообщения
+$(".btn.btn-default").click(function(){
+  $("#contact-form").hide(800); // скрывает форму
+  $(".alert.alert-success").show("slow"); // выводит скрытое сообщение
+});
